2992e44c365f82d9fdfa821ad3fd9b4e94a949a0
giovanni iannace <giovanniiannace56@gmail.com>
2020-11-18 15:33:07 -0300

    fibonacci feito

---
+++ fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero :");
+            int num;
+                int x=1, y=0, z=0;
+                    num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++){
+            z = x + y;
+            Console.WriteLine(z);
+            y = x;
+            x = z;
+                }
+                Console.ReadLine();
+        }
+    }
+}
